276e974265e1b354772e0ecfd28087e7f188f17d
Park <pjh6947@naver.com>
2021-10-07 17:23:36 +0900

    #10강 bcrypt 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds=10
+
 
 const userSchema=mongoose.Schema({
     name: {
@@ -27,6 +30,23 @@ const userSchema=mongoose.Schema({
 
     }
 })
+
+userSchema.pre('save', function(next){      //저장전 전처리하고 next함수 실행하면 다음으로 넘어간다.
+    //비밀번호를 암호화
+    var user=this;
+
+    if(user.isModified('password')){    //비밀번호가 수정될때만 암호화 해준다.
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err)return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash){   //hash가 암호화된 비번
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }
+})
+
 //스키마를 모델로 감싼다.
 const User = mongoose.model('User',userSchema);
 
