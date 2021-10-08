6f679b9e014a3bb52e767817d3906bb5f72229b0
Park <pjh6947@naver.com>
2021-10-08 23:11:41 +0900

    27강까지 들음

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
 import 'antd/dist/antd.css';
+import {applyMiddleware, createStore} from 'redux'; 
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import reducer from './_reducers';
+
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore) //원래는 createStore 만 해서 스토어를 리덕스에서 생성하는데 그냥 스토어는 객체밖에 못받아서 좌측 두개의 인자를 줌 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <Provider
+    store={createStoreWithMiddleware(reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__&&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+   > 
+     <App /> 
+   </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
