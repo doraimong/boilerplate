3a4488bc032f391b4e1132773f67a521ba837b6f
Park <pjh6947@naver.com>
2021-10-11 15:57:36 +0900

    #32 로그아웃페이지 구성 및 동작 완료 정상작동

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,12 +2,23 @@ import React,{useEffect} from 'react'
 import axios from 'axios';
 
 
-function LandingPage(){
+function LandingPage(props){
 
     useEffect(() => {
         axios.get('/api/hello') //서버로 보낸다.
         .then(response=>{console.log(response)})// 서버에서 가져온다.
     }, [])
+
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')  //바로 axios 사용/ server -> index.js get형식의 logout
+         .then(response=> {
+             if(response.data.success) {
+                 props.history.push("/login")   //로그아웃 후 로그인 페이지로 돌아간다.
+             } else {
+                 alert('로그아웃 하는데 실패 했습니다.')
+             }
+         })
+    }
 
     return(
         <div style={{
@@ -15,6 +26,9 @@ function LandingPage(){
             width: '100%', height:'100vh'
         }}>
             <h2>시작 페이지</h2>
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
